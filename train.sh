98f33f2443aa16666e780533d46a801e5d4e61f4
xjx <85392378+JinxiaXie@users.noreply.github.com>
2024-03-11 11:48:19 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python tracking/train.py --script aqatrack --config multi-hivit-ep150-4frames --save_dir ./output --mode multiple --nproc_per_node 4
-#python tracking/test_epoch.py
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tracking/train.py --script aqatrack --config multi-hivit-ep150-4frames --save_dir ./output --mode multiple --nproc_per_node 4
